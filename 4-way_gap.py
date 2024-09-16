3d1beefbc153e67e4c11db6d95b47b04c67f9e73
hendrixperalta <hendrixperalta@gmail.com>
2024-09-16 20:19:35 +0900

    Created a data frame that has the f1 scoring system

--- 4-way_gap.py
+++ 4-way_gap.py
@@ -1,1 +1,11 @@
 # %%
+import pandas as pd 
+
+# %%
+f1_points = [[1, 25], [2, 18], [3, 15], [4, 12], [5, 10],
+             [6, 8], [7, 6], [8, 4], [8, 2], [10, 1]]
+
+f_lap = 1
+
+point_system = pd.DataFrame(f1_points, columns=["pos", "point"])
+# %%
